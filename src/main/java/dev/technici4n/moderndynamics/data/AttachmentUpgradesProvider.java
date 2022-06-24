69726c1b664a468fb9af3f204569f68397b25203
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-06-24 12:22:56 +0200

    Fix #29: Unlock advanced behavior with comparators

--- src/main/java/dev/technici4n/moderndynamics/data/AttachmentUpgradesProvider.java
+++ src/main/java/dev/technici4n/moderndynamics/data/AttachmentUpgradesProvider.java
@@ -41,6 +41,7 @@ public class AttachmentUpgradesProvider implements DataProvider {
         vanillaUpgrade(Items.STICKY_PISTON, 1).itemSpeed(1);
         vanillaUpgrade(Items.HOPPER, 6).filterSlots(2).itemCount(5);
         vanillaUpgrade(Items.REPEATER, 3).itemTransferFrequency(1);
+        vanillaUpgrade(Items.COMPARATOR, 1).advancedBehavior();
 
         modUpgrade("ae2", "speed_card", 3).itemSpeed(1).itemTransferFrequency(1).fluidTransfer(0, 1);
         modUpgrade("ae2", "capacity_card", 6).filterSlots(2).itemCount(10).fluidTransfer(1, 0);
